dbf02f795dc39dd0c6c70b1f4e626cf9a9ea57e6
Unknown <andrewpetersell@gmail.com>
2017-04-03 23:19:03 -0400

    UI and DUNZO

---
+++ Week 9 Homework/Assets/Scripts/HealthDisplay.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthDisplay : MonoBehaviour {
+
+	public Text scoreBoard;
+	PlayerMovement pm;
+	Text t;
+
+	// Use this for initialization
+	void Start () {
+		pm = GetComponent <PlayerMovement> ();
+		t = scoreBoard.GetComponent<Text> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		t.text = pm.health.ToString (); 
+		
+	}
+}
